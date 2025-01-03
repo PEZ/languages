ca157168d2c16bbfebf20bc52bad403b64c4cf07
PEZ <pez@pezius.com>
2025-01-03 16:40:24 +0100

    Java: Use static class to host fibonacci function

--- fibonacci/jvm/code.java
+++ fibonacci/jvm/code.java
@@ -2,21 +2,23 @@ package jvm;
 
 public class code {
 
-    private static int fibonacci(int n) {
-        if (n == 0) {
-            return 0;
-        }
-        if (n == 1) {
-            return 1;
-        }
-        return fibonacci(n - 1) + fibonacci(n - 2);
+    private static class Fibonacci {
+        private static int fibonacci(int n) {
+            if (n == 0) {
+                return 0;
+            }
+            if (n == 1) {
+                return 1;
+            }
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }    
     }
 
     public static void main(String[] args) {
         var u = Integer.parseInt(args[0]);
         var r = 0;
         for (var i = 1; i < u; i++) {
-            r += fibonacci(i);
+            r += Fibonacci.fibonacci(i);
         }
         System.out.println(r);
     }
